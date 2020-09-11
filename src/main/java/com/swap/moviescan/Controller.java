af99732c816b84551906abda054d1104f1c2902e
Roman Poslavski <swapper9@gmail.com>
2020-09-11 18:39:33 +0300

    params are better ))

--- src/main/java/com/swap/moviescan/Controller.java
+++ src/main/java/com/swap/moviescan/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,10 +15,10 @@ public class Controller {
     @Autowired
     private KinoPoiskApi kinoPoiskApi;
 
-    @GetMapping(value = "/searchByKeyword/{keyword}/{page}")
+    @GetMapping(value = "/searchByKeyword")
     public ResponseEntity<SearchByKeywordResponse> searchByKeyword(
-      @PathVariable(value = "keyword") String keyword,
-      @PathVariable(value = "page") String page) {
+      @RequestParam(value = "keyword") String keyword,
+      @RequestParam(value = "page") String page) {
         return kinoPoiskApi.searchByKeyword(keyword, page);
     }
 
